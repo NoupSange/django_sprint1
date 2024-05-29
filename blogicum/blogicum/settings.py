76c782271c344bc4b4be0e90532f942637d6d6fb
Mikhail Fedoseev <fedoseevmvf@yandex.ru>
2024-05-30 01:28:35 +0300

    delete unused imported modules from files

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR / 'templates'
 STATICFILES_DIRS = [
     BASE_DIR / 'templates',
-] 
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
